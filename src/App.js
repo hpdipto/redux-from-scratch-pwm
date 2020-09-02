23ca80dc6f0c512de17204e124f8fa5cc7e9dd50
Haris Pasha Dipto <haris.dipto@gmail.com>
2020-09-02 11:48:10 +0600

    Store added with subscription, unsubscription

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import React from 'react';
 
-
+import store from './store';
 import './App.css';
 
 
 function App() {
+
+  const unsubscribe = store.subscribe(() => {
+    console.log("Store changed!", store.getState());
+  });
+
+
+  store.dispatch({
+    type: "bugAdded",
+    payload: {
+      description: "Bug1"
+    }
+  });
+
+  unsubscribe();
+
+  store.dispatch({
+    type: "bugRemoved",
+    payload: {
+      id: 1
+    }
+  })
+
+
+  console.log(store.getState());
+
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <h1>Hello Redux!</h1>
     </div>
   );
 }
